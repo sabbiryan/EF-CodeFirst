c54d9ba0b299d2bc072795f2fef07aeec6ae73da
Md. Sabbir Ahamed <sabbiryan@gmail.com>
2017-03-16 11:42:16 +0600

    foreign key referance added on employee model

--- Source/EF-CodeFirst/Model/Employee.cs
+++ Source/EF-CodeFirst/Model/Employee.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,11 +11,18 @@ namespace Model
     public class Employee: Entity
     {
         [Required]
+        public string CompanyId { get; set; }        
+
+        [Required]
         public string Name { get; set; }
 
         public string Designation { get; set; }
 
         [Required]
         public DateTime JoiningDate { get; set; }
+
+
+        [ForeignKey("CompanyId")]
+        public virtual Company Company { get; set; }
     }
 }
---
+++ Source/EF-CodeFirst/Model/Migrations/201703160541145_EmployeeModelAdded.cs
@@ -0,0 +1,38 @@
+namespace Model.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class EmployeeModelAdded : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Employees",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        CompanyId = c.String(nullable: false, maxLength: 128),
+                        Name = c.String(nullable: false),
+                        Designation = c.String(),
+                        JoiningDate = c.DateTime(nullable: false),
+                        Created = c.DateTime(nullable: false),
+                        CreatedBy = c.String(),
+                        Modified = c.DateTime(nullable: false),
+                        ModifiedBy = c.String(),
+                        IsActive = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Companies", t => t.CompanyId, cascadeDelete: true)
+                .Index(t => t.CompanyId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Employees", "CompanyId", "dbo.Companies");
+            DropIndex("dbo.Employees", new[] { "CompanyId" });
+            DropTable("dbo.Employees");
+        }
+    }
+}
